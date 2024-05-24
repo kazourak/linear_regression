256959079abce0aba975e6b65da3141c53b507e8
kazourak <nskiba@student.42angouleme.fr>
2024-05-24 14:14:25 +0200

    :sparkles: finish predict

--- ft_linear_regression/predict.py
+++ ft_linear_regression/predict.py
@@ -1,7 +1,16 @@
-def predict(thetas: tuple[float, float], mileage_to_predict: int = None):
-    mileage = 0
+def predict(thetas: tuple[float, float], mileage_to_predict: int = None) -> float:
+    """
+    Predict the price of a car given its mileage.
+    If no mileage is provided, ask the user for it.
+
+    Formula used to predict: ``y=theta0+theta1*mileage``
+    :param thetas:
+    :param mileage_to_predict:
+    :return prediction:
+    """
     theta0, theta1 = thetas
 
+    # If mileage is provided as an argument, use it. Otherwise, ask the user for it.
     if mileage_to_predict is not None:
         mileage = mileage_to_predict
     else:
@@ -10,4 +19,4 @@ def predict(thetas: tuple[float, float], mileage_to_predict: int = None):
             _mileage = input("Please enter a valid mileage: ")
         mileage = int(_mileage)
 
-    print(f"The estimated price of the car is: {theta0 + theta1 * int(mileage)}")
+    return theta0 + theta1 * mileage
--- predict_program.py
+++ predict_program.py
@@ -3,6 +3,10 @@ import argparse
 
 
 def parse_args() -> argparse.ArgumentParser:
+    """
+    Parse the arguments for the program with argparse
+    :return:
+    """
     parser = argparse.ArgumentParser(prog="predict_program.py", description="Linear regression program - Predict part", epilog="42 AI project")
     parser.add_argument("--theta", nargs=2, type=float, help="Thetas to use for prediction")
     parser.add_argument("--mileage", nargs=1, type=int, help="Mileage of the car to predict")
@@ -11,20 +15,34 @@ def parse_args() -> argparse.ArgumentParser:
 
 
 def get_thetas(args: argparse.Namespace) -> tuple[float, float]:
+    """
+    Get the thetas to use for prediction.
+    If thetas are provided as arguments, return them.
+    Otherwise, get the thetas from the file.
+
+    :param args:
+    :return:
+    """
     if args.theta is not None:
         return args.theta[0], args.theta[1]
     return ft.get_thetas()
 
 
 def main():
+    # Get and parse the arguments
     parser = parse_args()
     args = parser.parse_args()
+
+    # Get the thetas. If thetas are provided as arguments, use them. Otherwise, get the thetas from the file
     thetas = get_thetas(args)
 
+    # Save the thetas if the save argument is provided
     if args.save is not None and args.theta is not None:
         ft.set_thetas(args.theta[0], args.theta[1])
 
+    # Use the mileage provided as argument if it is provided
     mileage = args.mileage[0] if args.mileage is not None else None
+
     ft.predict(thetas, mileage)
 
 
